8ff26f7f084019ffa1dd422d8891cc672afeb762
gianluca <gianlucachiap@gmail.com>
2021-06-17 14:08:00 +0200

    fix: build sections

--- commands/build/index.tsx
+++ commands/build/index.tsx
@@ -1,11 +1,13 @@
 import { Box } from "ink";
 import React from "react";
 import BuildScripts from "./scripts";
+import BuildSections from "./sections";
 
 function Watch() {
 	return (
 		<Box flexDirection="column">
 			<BuildScripts />
+			<BuildSections />
 		</Box>
 	);
 }
